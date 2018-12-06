124e39d6cf02c49296c91cb1e937f0e6ff9e12a6
Pasha Kolesnyk <kolpal17@gmail.com>
2018-12-06 19:05:39 +0200

    Added lab04

--- Sharaga_3kurs/OOP/me/labs/c#/lab04/ConsoleApplication1/Program.cs
+++ Sharaga_3kurs/OOP/me/labs/c#/lab04/ConsoleApplication1/Program.cs
@@ -4,8 +4,101 @@ namespace ConsoleApplication1
 {
     internal class Program
     {
+        interface IDerzhava
+        {
+            uint Population { get; set; }
+            
+            string Name { get; set; }
+            
+            void Info();
+        }
+
+        class Republic : IDerzhava
+        {
+            public string Name { get; set; }
+
+            public uint Population { get; set; }
+            
+            public void Info()
+            {
+                Console.WriteLine("Republic " + Name + ", population = " + Population);
+            }
+
+            public Republic(string name, uint population)
+            {
+                Name = name;
+                Population = population;
+            }
+            
+            public Republic(){}
+        }
+        
+        class Monarchy : IDerzhava
+        {
+            public string Name { get; set; }
+
+            public uint Population { get; set; }
+            
+            public void Info()
+            {
+                Console.WriteLine("Monarchy " + Name + ", population = " + Population);
+            }
+
+            public Monarchy(string name, uint population)
+            {
+                Name = name;
+                Population = population;
+            }
+            
+            public Monarchy(){}
+        }
+        
+        class Kingdom : IDerzhava
+        {
+            public string Name { get; set; }
+
+            public uint Population { get; set; }
+            
+            public void Info()
+            {
+                Console.WriteLine("Kingdom " + Name + ", population = " + Population);
+            }
+
+            public Kingdom(string name, uint population)
+            {
+                Name = name;
+                Population = population;
+            }
+            
+            public Kingdom(){}
+        }
+        
+        
+        
         public static void Main(string[] args)
         {
+            Republic ukraine = new Republic();
+            Monarchy spain = new Monarchy("Spain", 70000000);
+            Kingdom uk = new Kingdom("United Kingdom", 140000000);
+            ukraine.Name = "Ukraine";
+            ukraine.Population = 41000000;
+            
+            ukraine.Info();
+            spain.Info();
+            uk.Info();
+
+            Console.WriteLine();
+            
+            IDerzhava[] masiv = new IDerzhava[3];
+
+            masiv[0] = ukraine;
+            masiv[1] = spain;
+            masiv[2] = uk;
+
+            foreach (IDerzhava derzh in masiv)
+            {
+                derzh.Info();
+            }
             Console.WriteLine("Hello, rider !");
         }
     }
